b5fff4ca459febd180cd097502f93cf54834db20
lwj <lwj0103@qq.com>
2025-05-05 22:50:28 +0800

    初始化提交

---
+++ jxnu-server/src/main/java/com/lwj/JxnuServerApplication.java
@@ -0,0 +1,13 @@
+package com.lwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxnuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxnuServerApplication.class, args);
+    }
+
+}
---
+++ jxnu-server/src/test/java/com/lwj/JxnuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxnuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
